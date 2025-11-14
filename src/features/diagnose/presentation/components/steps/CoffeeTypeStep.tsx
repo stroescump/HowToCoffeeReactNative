193d63dd2706f9792b3fc6923f2d41d247e9da89
Marius Stroescu <stroescump@gmail.com>
2025-11-14 22:49:45 +0100

    CoffeeTypeStep header

--- src/features/diagnose/presentation/components/steps/CoffeeTypeStep.tsx
+++ src/features/diagnose/presentation/components/steps/CoffeeTypeStep.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { CoffeeType } from "../../../domain/valueObjects/CoffeeType";
 
 type CoffeeTypeStepProps = {
-    value: CoffeeType;
-    onSubmit: (coffeeType: CoffeeType) => void;
+  value: CoffeeType;
+  onSubmit: (coffeeType: CoffeeType) => void;
 };
 
-export const CoffeeTypeStep = ({ value, onSubmit }: CoffeeTypeStepProps) => { 
-  return(
-    <View>
-      
+export const CoffeeTypeStep = ({ value, onSubmit }: CoffeeTypeStepProps) => {
+  return (
+    <View className="flex-1">
+      <Text className="font-semibold text-5xl tracking-[-3px] text-center">{`What kind of coffee\nare you using?`}</Text>
     </View>
   )
- };
+};
